b13d43a82edb26e352ccd881e78c70022a8eed72
Jan Snasel <jan.snasel@networktocode.com>
2023-12-05 14:21:22 +0000

    chore: Nautobot 2.x

--- tasks.py
+++ tasks.py
@@ -46,7 +46,7 @@ namespace = Collection("nautobot_netbox_importer")
 namespace.configure(
     {
         "nautobot_netbox_importer": {
-            "nautobot_ver": "1.6.0",
+            "nautobot_ver": "2.0.0",
             "project_name": "nautobot-netbox-importer",
             "python_ver": "3.11",
             "local": False,
