0ecb87969d9ad59e00f297d4f1079f3844dd25c1
Jan Snasel <jan.snasel@networktocode.com>
2023-12-18 17:15:38 +0000

    chore: Update Nautobot minimal to 2.0.6

--- nautobot_netbox_importer/__init__.py
+++ nautobot_netbox_importer/__init__.py
@@ -17,7 +17,7 @@ class NautobotNetboxImporterConfig(NautobotAppConfig):
     description = "Data importer from NetBox 2.10.x to Nautobot."
     base_url = "netbox-importer"
     required_settings = []
-    min_version = "2.0.0"
+    min_version = "2.0.6"
     max_version = "2.9999"
     default_settings = {}
     caching_config = {}
--- tasks.py
+++ tasks.py
@@ -46,7 +46,7 @@ namespace = Collection("nautobot_netbox_importer")
 namespace.configure(
     {
         "nautobot_netbox_importer": {
-            "nautobot_ver": "2.0.0",
+            "nautobot_ver": "2.0.6",
             "project_name": "nautobot-netbox-importer",
             "python_ver": "3.11",
             "local": False,
