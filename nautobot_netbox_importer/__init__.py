c85a0bf2e1e50db2cca15d85b94c581c93483b4b
bakebot <info@networktocode.com>
2024-01-17 13:57:26 +0000

    Cookie updated by NetworkToCode Cookie Drift Manager Tool
    Template:
    
    ```
    {
        "template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
        "dir": "nautobot-app",
        "ref": "u/snaselj-drift-manager",
        "path": null
    }
    ```
    
    Cookie:
    
    ```
    {
        "remote": "https://github.com/nautobot/nautobot-app-netbox-importer.git",
        "path": "/tmp/tmpdsey2mgl/nautobot-app-netbox-importer",
        "repository_path": "/tmp/tmpdsey2mgl/nautobot-app-netbox-importer",
        "dir": "",
        "branch_prefix": "drift-manager",
        "context": {
            "codeowner_github_usernames": "@glennmatthews @jathanism @lampwins @chadell",
            "full_name": "Network to Code, LLC",
            "email": "opensource@networktocode.com",
            "github_org": "nautobot",
            "app_name": "nautobot_netbox_importer",
            "verbose_name": "Nautobot NetBox Importer",
            "app_slug": "nautobot-netbox-importer",
            "project_slug": "nautobot-app-netbox-importer",
            "repo_url": "https://github.com/nautobot/nautobot-app-netbox-importer",
            "base_url": "netbox-importer",
            "min_nautobot_version": "1.6.0",
            "max_nautobot_version": "1.9999",
            "camel_name": "NautobotNetboxImporter",
            "project_short_description": "Data importer from NetBox 2.10.x to Nautobot.",
            "model_class_name": "None",
            "open_source_license": "Apache-2.0",
            "docs_base_url": "https://docs.nautobot.com",
            "docs_app_url": "https://docs.nautobot.com/projects/netbox-importer/en/latest",
            "_template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
            "_output_dir": "/tmp/tmpdsey2mgl",
            "_repo_dir": "/github/home/.cookiecutters/cookiecutter-nautobot-app/nautobot-app",
            "_checkout": "u/snaselj-drift-manager"
        },
        "base_branch": "develop",
        "remote_name": "origin",
        "pull_request_strategy": "PullRequestStrategy.CREATE",
        "post_actions": [
            "PostAction.BLACK"
        ],
        "baked_commit_ref": "8049fdf6443472afd47288205d8614c89d7b2c72",
        "draft": true
    }
    ```
    
    CLI Arguments:
    
    ```
    {
        "cookie_dir": "",
        "input": false,
        "json_filename": "",
        "output_dir": "",
        "push": true,
        "template": "",
        "template_dir": "",
        "template_ref": "u/snaselj-drift-manager",
        "pull_request": null,
        "post_action": [],
        "disable_post_actions": false,
        "draft": true
    }
    ```

--- nautobot_netbox_importer/__init__.py
+++ nautobot_netbox_importer/__init__.py
@@ -1,14 +1,14 @@
-"""Plugin declaration for nautobot_netbox_importer."""
+"""App declaration for nautobot_netbox_importer."""
 # Metadata is inherited from Nautobot. If not including Nautobot in the environment, this should be added
 from importlib import metadata
 
-__version__ = metadata.version(__name__)
+from nautobot.apps import NautobotAppConfig
 
-from nautobot.extras.plugins import NautobotAppConfig
+__version__ = metadata.version(__name__)
 
 
 class NautobotNetboxImporterConfig(NautobotAppConfig):
-    """Plugin configuration for the nautobot_netbox_importer plugin."""
+    """App configuration for the nautobot_netbox_importer app."""
 
     name = "nautobot_netbox_importer"
     verbose_name = "Nautobot NetBox Importer"
