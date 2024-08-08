d497fdc1a5c4fcb260a0e81ad4847806632f41bf
bakebot <info@networktocode.com>
2024-08-08 18:27:38 +0000

    Cookie updated by NetworkToCode Cookie Drift Manager Tool
    Template:
    
    ```
    {
        "template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
        "dir": "nautobot-app",
        "ref": "refs/tags/nautobot-app-v2.3.0",
        "path": null
    }
    ```
    
    Cookie:
    
    ```
    {
        "remote": "https://github.com/nautobot/nautobot-app-netbox-importer.git",
        "path": "/tmp/tmp9ojfjy0x/nautobot-app-netbox-importer",
        "repository_path": "/tmp/tmp9ojfjy0x/nautobot-app-netbox-importer",
        "dir": "",
        "branch_prefix": "drift-manager",
        "context": {
            "codeowner_github_usernames": "@glennmatthews @jathanism @lampwins @chadell",
            "full_name": "Network to Code, LLC",
            "email": "opensource@networktocode.com",
            "github_org": "nautobot",
            "app_name": "nautobot_netbox_importer",
            "verbose_name": "Nautobot NetBox Importer",
            "app_slug": "nautobot-netbox-importer",
            "project_slug": "nautobot-app-netbox-importer",
            "repo_url": "https://github.com/nautobot/nautobot-app-netbox-importer",
            "base_url": "netbox-importer",
            "min_nautobot_version": "2.0.6",
            "max_nautobot_version": "2.9999",
            "camel_name": "NautobotNetboxImporter",
            "project_short_description": "Data importer from NetBox 3.x to Nautobot 2.x.",
            "model_class_name": "None",
            "open_source_license": "Apache-2.0",
            "docs_base_url": "https://docs.nautobot.com",
            "docs_app_url": "https://docs.nautobot.com/projects/netbox-importer/en/latest",
            "_template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
            "_output_dir": "/tmp/tmp9ojfjy0x",
            "_repo_dir": "/github/home/.cookiecutters/cookiecutter-nautobot-app/nautobot-app",
            "_checkout": "refs/tags/nautobot-app-v2.3.0"
        },
        "base_branch": "develop",
        "remote_name": "origin",
        "pull_request_strategy": "PullRequestStrategy.CREATE",
        "post_actions": [
            "PostAction.BLACK"
        ],
        "baked_commit_ref": "4ac68ac681124f94787b81fdee4602b828985473",
        "draft": true
    }
    ```
    
    CLI Arguments:
    
    ```
    {
        "cookie_dir": "",
        "input": false,
        "json_filename": "",
        "output_dir": "",
        "push": true,
        "template": "",
        "template_dir": "",
        "template_ref": "refs/tags/nautobot-app-v2.3.0",
        "pull_request": null,
        "post_action": [],
        "disable_post_actions": false,
        "draft": null
    }
    ```

--- nautobot_netbox_importer/__init__.py
+++ nautobot_netbox_importer/__init__.py
@@ -1,4 +1,5 @@
 """App declaration for nautobot_netbox_importer."""
+
 # Metadata is inherited from Nautobot. If not including Nautobot in the environment, this should be added
 from importlib import metadata
 
@@ -14,7 +15,7 @@ class NautobotNetboxImporterConfig(NautobotAppConfig):
     verbose_name = "Nautobot NetBox Importer"
     version = __version__
     author = "Network to Code, LLC"
-    description = "Data importer from NetBox 2.10.x to Nautobot.."
+    description = "Data importer from NetBox 3.x to Nautobot 2.x.."
     base_url = "netbox-importer"
     required_settings = []
     min_version = "2.0.6"
