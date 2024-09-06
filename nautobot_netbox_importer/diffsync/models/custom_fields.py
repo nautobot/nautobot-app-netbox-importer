28a65d32bd75b590614c090ffe706fc9c027367e
Gary Snider <75227981+gsnider2195@users.noreply.github.com>
2024-09-06 15:04:04 -0700

    move common code to utils

--- nautobot_netbox_importer/diffsync/models/custom_fields.py
+++ nautobot_netbox_importer/diffsync/models/custom_fields.py
@@ -1,7 +1,7 @@
 """NetBox to Nautobot Custom Fields Models Mapping."""
 
 import json
-from typing import Any, Generator, Iterable, Tuple
+from typing import Any, Iterable
 
 from nautobot_netbox_importer.base import RecordData, Uid
 from nautobot_netbox_importer.generator import (
@@ -13,6 +13,7 @@ from nautobot_netbox_importer.generator import (
     SourceField,
     fields,
 )
+from nautobot_netbox_importer.utils import get_field_choices
 
 
 def _convert_choices(choices: Any) -> list:
@@ -54,16 +55,6 @@ def _convert_choices(choices: Any) -> list:
     return choices
 
 
-# TODO: Borrowed code from SourceField.set_choice_importer. Can this code be re-used without copy/pasting?
-def get_choices(items: Iterable) -> Generator[Tuple[Any, Any], None, None]:
-    """Get choices from a field."""
-    for key, value in items:
-        if isinstance(value, (list, tuple)):
-            yield from get_choices(value)
-        else:
-            yield key, value
-
-
 def _define_custom_field_type(field: SourceField) -> None:
     """Define the custom field `type` field importer.
 
@@ -78,12 +69,11 @@ def _define_custom_field_type(field: SourceField) -> None:
         NetBox type "multiselect" must be converted to "multi-select" in Nautobot. Fall back to "text" for all unknown field types.
         """
         # Process the conversion from NetBox to Nautobot
-        # TODO: Borrowed code from SourceField.set_choice_importer. Can this code be re-used without copy/pasting?
         field_choices = getattr(field.nautobot.field, "choices", None)
         if not field_choices:
             raise ValueError(f"Invalid field_choices for {field}")
 
-        choices = dict(get_choices(field_choices))
+        choices = dict(get_field_choices(field_choices))
         value = field.get_source_value(source)
         if value in choices:
             field.set_nautobot_value(target, value)
--- nautobot_netbox_importer/utils.py
+++ nautobot_netbox_importer/utils.py
@@ -1,5 +1,7 @@
 """Utility functions and classes for nautobot_netbox_importer."""
 
+from typing import Any, Generator, Iterable, Tuple
+
 from tqdm import tqdm
 
 
@@ -27,3 +29,12 @@ class ProgressBar(tqdm):
             self.set_description(stage)
             self.reset(total=total)
         return self.update(current - self.n)
+
+
+def get_field_choices(items: Iterable) -> Generator[Tuple[Any, Any], None, None]:
+    """Yield all choices from a model field, flattening nested iterables."""
+    for key, value in items:
+        if isinstance(value, (list, tuple)):
+            yield from get_field_choices(value)
+        else:
+            yield key, value
