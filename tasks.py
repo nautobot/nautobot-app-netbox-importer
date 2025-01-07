0af2f9fa402e30718278334d3ce29bc77d4f9fd0
bakebot <info@networktocode.com>
2025-01-07 18:42:17 +0000

    Cookie updated by NetworkToCode Cookie Drift Manager Tool
    Template:
    
    ```
    {
        "template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
        "dir": "nautobot-app",
        "ref": "refs/tags/nautobot-app-v2.4.1",
        "path": null
    }
    ```
    
    Cookie:
    
    ```
    {
        "remote": "https://github.com/nautobot/nautobot-app-netbox-importer.git",
        "path": "/tmp/tmpjedm8hdw/nautobot-app-netbox-importer",
        "repository_path": "/tmp/tmpjedm8hdw/nautobot-app-netbox-importer",
        "dir": "",
        "branch_prefix": "drift-manager",
        "context": {
            "codeowner_github_usernames": "@glennmatthews @jathanism @lampwins @chadell",
            "full_name": "Network to Code, LLC",
            "email": "opensource@networktocode.com",
            "github_org": "nautobot",
            "app_name": "nautobot_netbox_importer",
            "verbose_name": "Nautobot NetBox Importer",
            "app_slug": "nautobot-netbox-importer",
            "project_slug": "nautobot-app-netbox-importer",
            "repo_url": "https://github.com/nautobot/nautobot-app-netbox-importer",
            "base_url": "netbox-importer",
            "min_nautobot_version": "2.0.6",
            "max_nautobot_version": "2.9999",
            "camel_name": "NautobotNetboxImporter",
            "project_short_description": "Data importer from NetBox 3.x to Nautobot 2.x.",
            "model_class_name": "None",
            "open_source_license": "Apache-2.0",
            "docs_base_url": "https://docs.nautobot.com",
            "docs_app_url": "https://docs.nautobot.com/projects/netbox-importer/en/latest",
            "_template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
            "_output_dir": "/tmp/tmpjedm8hdw",
            "_repo_dir": "/github/home/.cookiecutters/cookiecutter-nautobot-app/nautobot-app",
            "_checkout": "refs/tags/nautobot-app-v2.4.1"
        },
        "base_branch": "develop",
        "remote_name": "origin",
        "pull_request_strategy": "PullRequestStrategy.CREATE",
        "post_actions": [
            "PostAction.RUFF",
            "PostAction.POETRY"
        ],
        "baked_commit_ref": "55b82598c1e369090711b64d1f97f7567367664b",
        "draft": false
    }
    ```
    
    CLI Arguments:
    
    ```
    {
        "cookie_dir": "",
        "input": false,
        "json_filename": "",
        "output_dir": "",
        "push": true,
        "template": "",
        "template_dir": "",
        "template_ref": "refs/tags/nautobot-app-v2.4.1",
        "pull_request": null,
        "post_action": [
            "ruff",
            "poetry"
        ],
        "disable_post_actions": true,
        "draft": false
    }
    ```

--- tasks.py
+++ tasks.py
@@ -165,7 +165,7 @@ def docker_compose(context, command, **kwargs):
     return context.run(compose_command, env=build_env, **kwargs)
 
 
-def run_command(context, command, **kwargs):
+def run_command(context, command, service="nautobot", **kwargs):
     """Wrapper to run a command locally or inside the nautobot container."""
     if is_truthy(context.nautobot_netbox_importer.local):
         if "command_env" in kwargs:
@@ -175,7 +175,7 @@ def run_command(context, command, **kwargs):
             }
         return context.run(command, **kwargs)
     else:
-        # Check if nautobot is running, no need to start another nautobot container to run a command
+        # Check if service is running, no need to start another container to run a command
         docker_compose_status = "ps --services --filter status=running"
         results = docker_compose(context, docker_compose_status, hide="out")
 
@@ -185,10 +185,10 @@ def run_command(context, command, **kwargs):
             for key, value in command_env.items():
                 command_env_args += f' --env="{key}={value}"'
 
-        if "nautobot" in results.stdout:
-            compose_command = f"exec{command_env_args} nautobot {command}"
+        if service in results.stdout:
+            compose_command = f"exec{command_env_args} {service} {command}"
         else:
-            compose_command = f"run{command_env_args} --rm --entrypoint='{command}' nautobot"
+            compose_command = f"run{command_env_args} --rm --entrypoint='{command}' {service}"
 
         pty = kwargs.pop("pty", True)
 
@@ -427,10 +427,14 @@ def shell_plus(context):
     run_command(context, command)
 
 
-@task
-def cli(context):
-    """Launch a bash shell inside the Nautobot container."""
-    run_command(context, "bash")
+@task(
+    help={
+        "service": "Docker compose service name to launch cli in (default: nautobot).",
+    }
+)
+def cli(context, service="nautobot"):
+    """Launch a bash shell inside the container."""
+    run_command(context, "bash", service=service)
 
 
 @task(
@@ -752,7 +756,8 @@ def pylint(context):
     else:
         print("No migrations directory found, skipping migrations checks.")
 
-    raise Exit(code=exit_code)
+    if exit_code != 0:
+        raise Exit(code=exit_code)
 
 
 @task(aliases=("a",))
@@ -796,7 +801,8 @@ def ruff(context, action=None, target=None, fix=False, output_format="concise"):
         if not run_command(context, command, warn=True):
             exit_code = 1
 
-    raise Exit(code=exit_code)
+    if exit_code != 0:
+        raise Exit(code=exit_code)
 
 
 @task
