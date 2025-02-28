8ce693ab47d20c2a35ffeec4a23557159f28ea38
Stephen Kiely <stephenkiely87@gmail.com>
2025-02-28 10:13:39 -0600

    Fix after drift-manager, update change fragments for Towncrier.

--- nautobot_netbox_importer/urls.py
+++ nautobot_netbox_importer/urls.py
@@ -3,19 +3,8 @@
 from django.templatetags.static import static
 from django.urls import path
 from django.views.generic import RedirectView
-from nautobot.apps.urls import NautobotUIViewSetRouter
-
-
-# Uncomment the following line if you have views to import
-# from nautobot_netbox_importer import views
-
 
 app_name = "nautobot_netbox_importer"
-router = NautobotUIViewSetRouter()
-
-# Here is an example of how to register a viewset, you will want to replace views.NautobotNetboxImporterUIViewSet with your viewset
-# router.register("nautobot_netbox_importer", views.NautobotNetboxImporterUIViewSet)
-
 
 urlpatterns = [
     path("docs/", RedirectView.as_view(url=static("nautobot_netbox_importer/docs/index.html")), name="docs"),
